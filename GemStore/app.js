bfedd530954fcdf09f0b303ced53f7a9b1e14b94
Eze <eze@eze-CX215XX-CX212XX.(none)>
2015-10-29 18:32:29 -0300

    fix ejercicio 4

--- GemStore/app.js
+++ GemStore/app.js
@@ -1,11 +1,13 @@
-angular
-	.module('gemStore', [])
-	.controller('StoreController', function () {
-		var gems = [
-      		{ name: 'Azurite', price: 2.95 },
-      		{ name: 'Bloodstone', price: 5.95 },
-      		{ name: 'Zircon', price: 3.95 },
-    	];
-        
-        this.products = gems;
-    });
+(function () {
+	var gems = [
+      			{ name: 'Azurite', price: 2.95 },
+      			{ name: 'Bloodstone', price: 5.95 },
+      			{ name: 'Zircon', price: 3.95 },
+    		];
+
+	angular
+		.module('gemStore', [])
+		.controller('StoreController', function () {
+        	this.products = gems;
+    	});
+})();
