159038e9ac57e11598f2103d0d70a7475d60edd0
ezequiel-gomez <eze.angel.gomez@gmail.com>
2015-10-29 17:32:11 -0300

    ejercicio 2

--- GemStore/app.js
+++ GemStore/app.js
@@ -1,1 +1,7 @@
-angular.module('gemStore', []);
+angular
+	.module('gemStore', [])
+	.controller('StoreController', function () {
+		var gem = { name: 'Azurite', price: 2.83 };
+        
+        this.product = gem;
+    });
